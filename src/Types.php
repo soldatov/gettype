2f2416864e5a3a5f59980bc50058e1b081278367
Anatoly Soldatov <soldatov.anatoly@gmail.com>
2020-09-15 22:03:27 +0400

    Realization.

---
+++ src/Types.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Soldatov\GetType;
+
+/**
+ * Collection of types that the function gettype() returns.
+ * */
+class Types
+{
+    const TYPE_BOOLEAN = 'boolean';
+    const TYPE_INTEGER = 'integer';
+    const TYPE_FLOAT = 'double';
+    const TYPE_STRING = 'string';
+    const TYPE_ARRAY = 'array';
+    const TYPE_OBJECT = 'object';
+    const TYPE_RESOURCE_OPEN = 'resource';
+    const TYPE_RESOURCE_CLOSED = 'resource (closed)';
+    const TYPE_NULL = 'NULL';
+
+    /** @deprecated */
+    const TYPE_RESOURCE_UNKNOWN = 'unknown type';
+}
